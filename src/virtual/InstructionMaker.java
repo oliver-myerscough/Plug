b6c1e5a866d0a4f935e78baa7e5c9cb8ae4a2143
Oliver Myerscough <oliver.myerscough@gmail.com>
2012-02-10 19:44:52 +0000

    updated instruction maker for instuction changes. reorganised to recognise the difference between direct and indirect instructions

--- src/virtual/InstructionMaker.java
+++ src/virtual/InstructionMaker.java
@@ -1,9 +1,14 @@
 package virtual;
+import instructions.AddDirect;
 import instructions.AddIndirect;
 import instructions.Goto;
+import instructions.LoadDirect;
 import instructions.LoadIndirect;
 import instructions.Stop;
+import instructions.StoreDirect;
 import instructions.StoreIndirect;
+import instructions.SubDirect;
+import instructions.SubIndirect;
 
 import java.awt.List;
 import java.util.HashMap;
@@ -22,16 +27,36 @@ public class InstructionMaker {
 		this.symbol = symbol;
 	}
 	
-	private int resolveAddr(String s) {
-		
-		String stripped;
-		if(s.startsWith("[") && s.endsWith("]")) {
-			stripped = s.substring(1, s.length()-1);
-		} else {
-			return -1; // invalid
+	private String stripAddressString(String s) {
+		return s.substring(1, s.length()-1);
+	}
+	
+	private Integer getRegisterAddr(String s) {
+		if(s.equals("a")) {
+			return 0;
+		} else if(s.equals("b")) {
+			return 1;
+		} else if(s.equals("c")) {
+			return 2;
+		} else if(s.equals("d")) {
+			return 3;
 		}
+		return null; // not a register
+	}
+	
+	private Integer getMemAddr(String s) {
 		
-		Matcher matcher = Pattern.compile("[0-9]+|[a-z]+|\\+|\\-").matcher(stripped);
+		try {
+			return Integer.parseInt(s);
+		} catch(NumberFormatException e) {
+			// not an integer, try to resolve with labels
+			return resolveAddr(s);
+		}
+	}
+	
+	private Integer resolveAddr(String s) {
+		
+		Matcher matcher = Pattern.compile("[0-9]+|[a-z]+|\\+|\\-").matcher(s);
 		int acc = 0;
 		LinkedList<Integer> valList = new LinkedList<Integer>();
 		LinkedList<String> opList = new LinkedList<String>();
@@ -50,6 +75,7 @@ public class InstructionMaker {
 			} else {
 				// error
 				System.out.println("encountered an error on token " + tok + " of " + s);
+				return null;
 			}
 			
 		}
@@ -58,11 +84,16 @@ public class InstructionMaker {
 		
 		for(int nVal : valList) {
 			String tok = opList.poll();
+			
+			if(tok == null) {
+				return null;
+			}
+			
 			if(tok.equals("+")) {
 				acc += nVal;
 			} else if (tok.equals("-")) {
 				acc -= nVal;
-			}
+			} 
 		}
 
 		return acc; 
@@ -92,48 +123,78 @@ public class InstructionMaker {
 			addr = Integer.parseInt(s);
 			return new Goto(addr);
 		} catch (NumberFormatException e) {
-			addr = resolveAddr(s);
-			// return new GotoM(addr);
+			// this shouldn't happen
 		}
 		return null;
 	}
 
 	public Data create_add(Scanner scanner){
 		int r1 = scanner.nextInt();
-		String s = scanner.next();
-		try {
-			int r2 = Integer.parseInt(s);
+		String s = stripAddressString(scanner.next());
+		
+		Integer r2 = getRegisterAddr(s);
+		if(r2 != null) {
 			return new AddIndirect(r1, r2);
-		} catch (NumberFormatException e) {
-			int addr = resolveAddr(s);
-			// return new AddM(r1, addr);
 		}
+		
+		Integer addr = getMemAddr(s);
+		if(addr != null) {
+			return new AddDirect(r1, addr);
+		}
+		
 		return null;
+		
+	}
+	
+	public Data create_sub(Scanner scanner){
+		int r1 = scanner.nextInt();
+		String s = stripAddressString(scanner.next());
+		
+		Integer r2 = getRegisterAddr(s);
+		if(r2 != null) {
+			return new SubIndirect(r1, r2);
+		}
+		
+		Integer addr = getMemAddr(s);
+		if(addr != null) {
+			return new SubDirect(r1, addr);
+		}
+		
+		return null;
+		
 	}
 	
 	public Data create_load(Scanner scanner){
 		int r1 = scanner.nextInt();
-		String s = scanner.next();
-		try {
-			int r2 = Integer.parseInt(s);
+		String s = stripAddressString(scanner.next());
+		
+		Integer r2 = getRegisterAddr(s);
+		if(r2 != null) {
 			return new LoadIndirect(r1, r2);
-		} catch (NumberFormatException e) {
-			int addr = resolveAddr(s);
-			// return new LoadM(r1, addr);
 		}
+		
+		Integer addr = getMemAddr(s);
+		if(addr != null) {
+			return new LoadDirect(r1, addr);
+		}
+		
 		return null;
 	}
 	
 	public Data create_store(Scanner scanner){
 		int r1 = scanner.nextInt();
-		String s = scanner.next();
-		try {
-			int r2 = Integer.parseInt(s);
+		String s = stripAddressString(scanner.next());
+		
+		Integer r2 = getRegisterAddr(s);
+		if(r2 != null) {
 			return new StoreIndirect(r1, r2);
-		} catch (NumberFormatException e) {
-			int addr = resolveAddr(s);
-			// return new StoreM(r1, addr);
 		}
+		
+		Integer addr = getMemAddr(s);
+		if(addr != null) {
+			return new StoreDirect(r1, addr);
+		}
+		
 		return null;
 	}
 	
